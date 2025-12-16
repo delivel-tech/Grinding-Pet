62b8969875c7d67e5ee90c70093bba49d290b22d
delivel <ytdeli924@gmail.com>
2025-12-15 23:44:00 -0800

    commited 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
+
+class $modify(PetMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+        
+        auto rightMenu = getChildByIDRecursive("right-side-menu");
+
+        auto spr = CCSprite::createWithSpriteFrameName("GJ_plainBtn_001.png");
+
+        auto btn = CCMenuItemSpriteExtra::create(spr, this, menu_selector(PetMenuLayer::onPetButton));
+
+        rightMenu->addChild(btn);
+        rightMenu->updateLayout();
+
+        return true;
+    };
+
+    void onPetButton(CCObject* sender) {
+        Notification::create("Callback :)", NotificationIcon::Success)->show();
+    }
+};
